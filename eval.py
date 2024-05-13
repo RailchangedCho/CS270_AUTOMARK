4d88fc9a45633d2ab79682a3b043ff12d50f0be5
Dongkuk Cho <dongkuk4888@kaist.ac.kr>
2024-05-13 10:38:09 +0900

    240513 [FIN(RAW)]

--- eval.py
+++ eval.py
@@ -1,17 +1,26 @@
 import quest as question
 import ans as answer
 
-q_result = question.getquestion()
-a_result = answer.getanswer()
+def eval():
+    q_result = question.getquestion()
+    a_result = answer.getanswer()
 
-print(q_result)
-print(a_result)
+    print("Q :" + str(q_result))
+    print("A :" + str(a_result))
 
-for i in range(10):
-    if q_result[i] == a_result[i]:
-        print("Problem " + str(i) + " correct")
-    else:
-        print("Problem " + str(i) + " incorrect")
+    AnsOrNot = []
+    for i in range(10):
+        if q_result[i] == a_result[i]:
+            print("Problem " + str(i+1) + " correct")
+            AnsOrNot.append(True)
+        else:
+            print("Problem " + str(i+1) + " incorrect")
+            AnsOrNot.append(False)
+
+    print(AnsOrNot)
+    return AnsOrNot
+
+eval()
 
 
 
